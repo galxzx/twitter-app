8659aa9ba11b5f7e7f5674926b364d5e0bb40247
Corryn Young <young.corryn@gmail.com>
2017-01-17 11:47:57 -0500

    after Live Test

--- app.js
+++ app.js
@@ -2,50 +2,25 @@ const express = require ('express');
 const app = express();
 const volleyBall = require ('volleyball');
 const nunjucks = require ('nunjucks');
+const routes = require ('./routes/');
 
-// app.use('/', function(req, res, next){
-//   console.log(req.method, req.originalUrl);
-//   next();
-// });
+// Setup
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+app.use(volleyBall); // Logger
+app.use(express.static('public'));
 
-
-
-// nunjucks.configure('views', {noCache:true});
-// nunjucks.render('index.html', locals, function(err, output){
-//   if(err) console.error(err);
-//   console.log('here', output);
-// });
-
-app.set('view engine', 'html'); // have res.render work with html files
-app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
-nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
-
-
-
-app.use(volleyBall);
-
+// Routes
+app.use('/', routes);
 
 app.use('/special', function(req, res, next){
   console.log('you reached the special area');
   next();
 });
 
-app.get('/', function(req, res, next){
-  const people = [
-    {name: 'Full'},
-    {name: 'Stacker'},
-    {name: 'Son'},
-    {name: 'Joe'}
-  ];
-  res.render(
-    'index',
-    {
-      title: 'Hall of Fame',
-      people: people
-    }
-  );
-})
 
+// Listen
 app.listen(3000, function(){
   console.log('Listening on port 3000');
 })
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+// could use one line instead: const router = require('express').Router();
+const tweetBank = require('../tweetBank');
+const path = require('path');
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/stylesheets/style.css', function(req, res){
+  res.sendFile(path.join(__dirname, '../public/stylesheets/style.css'));
+})
+
+module.exports = router;
