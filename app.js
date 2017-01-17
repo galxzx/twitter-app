9ce15d11888a966c6a39eb1a44eca098f51518df
Corryn Young <young.corryn@gmail.com>
2017-01-16 21:43:05 -0500

    after configuring express

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require ('express');
 const app = express();
+const volleyBall = require ('volleyball');
+
+// app.use('/', function(req, res, next){
+//   console.log(req.method, req.originalUrl);
+//   next();
+// });
+
+app.use(volleyBall);
+
+
+app.use('/special', function(req, res, next){
+  console.log('you reached the special area');
+  next();
+});
 
 app.get('/', function(req, res, next){
   res.send('Welcome to the twitter app\n');
