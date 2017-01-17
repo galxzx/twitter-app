e61c8f71b3259b7bfd5dc3696d7dd41fa3f1d4d3
Corryn Young <young.corryn@gmail.com>
2017-01-17 11:15:47 -0500

    saving

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require ('express');
 const app = express();
 const volleyBall = require ('volleyball');
+const nunjucks = require ('nunjucks');
 
 // app.use('/', function(req, res, next){
 //   console.log(req.method, req.originalUrl);
 //   next();
 // });
+
+
+
+// nunjucks.configure('views', {noCache:true});
+// nunjucks.render('index.html', locals, function(err, output){
+//   if(err) console.error(err);
+//   console.log('here', output);
+// });
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true}); // point nunjucks to the proper directory for templates
+
+
 
 app.use(volleyBall);
 
@@ -16,7 +31,19 @@ app.use('/special', function(req, res, next){
 });
 
 app.get('/', function(req, res, next){
-  res.send('Welcome to the twitter app\n');
+  const people = [
+    {name: 'Full'},
+    {name: 'Stacker'},
+    {name: 'Son'},
+    {name: 'Joe'}
+  ];
+  res.render(
+    'index',
+    {
+      title: 'Hall of Fame',
+      people: people
+    }
+  );
 })
 
 app.listen(3000, function(){
